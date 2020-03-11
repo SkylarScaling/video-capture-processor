3da98d7dfa34e72b0adaa67d406e713ae06d5451
Skylar Scaling <skylar.scaling@gmail.com>
2020-03-11 14:00:48 -0600

    Initial setup of capture processor structure

---
+++ src/main/java/com/wheezy/apps/vidcapproc/VideoCaptureProcessor.java
@@ -0,0 +1,36 @@
+package com.wheezy.apps.vidcapproc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class VideoCaptureProcessor extends Application
+{
+
+  @Override
+  public void start(Stage primaryStage)
+  {
+    try
+    {
+      // Read file fxml and draw interface.
+      Parent root = FXMLLoader.load(getClass().getResource("VideoCaptureProcessor.fxml"));
+
+      primaryStage.setTitle("Hell World FX!");
+      primaryStage.setScene(new Scene(root));
+      primaryStage.show();
+
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
+
+}
