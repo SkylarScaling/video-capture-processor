1b775f768cd20617159a8b214f3f888ae0000233
Skylar Scaling <skylar.scaling@gmail.com>
2020-04-21 17:06:06 -0600

    Settings icon now launches settings dialog.

---
+++ src/main/java/com/wheezy/apps/vidcapproc/controller/VideoCaptureProcessorGUIController.java
@@ -0,0 +1,60 @@
+package com.wheezy.apps.vidcapproc.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.wheezy.apps.vidcapproc.data.VCPSettings;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class VideoCaptureProcessorGUIController implements Initializable
+{
+
+  @FXML
+  private Button myButton;
+  @FXML
+  private TextField myTextField;
+
+  private VCPSettings vcpSettings;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources)
+  {
+    // TODO (don't really need to do anything here).
+  }
+
+  @FXML
+  void openSettingsDialog(ActionEvent event)
+  {
+    System.out.println("Button Clicked!");
+    
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../VCPSettingsDialog.fxml"));
+    Parent parent;
+    try
+    {
+      parent = fxmlLoader.load();
+      VCPSettingsDialogController dialogController = fxmlLoader.<VCPSettingsDialogController>getController();
+      dialogController.setVCPSettings(vcpSettings);
+      Scene scene = new Scene(parent);
+      Stage stage = new Stage();
+      stage.initModality(Modality.APPLICATION_MODAL);
+      stage.setScene(scene);
+      stage.showAndWait();
+    }
+    catch (IOException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+}
